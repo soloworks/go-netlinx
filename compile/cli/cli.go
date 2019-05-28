1e6eacb06dbead9b0d5add67a8e4f46347b07437
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-05-28 13:10:28 +0100

    Added CLI Tool

---
+++ compile/cli/cli.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"os"
+
+	"github.com/soloworks/go-netlinx/apw"
+	"github.com/soloworks/go-netlinx/compile"
+)
+
+type myargs struct {
+	Source string
+	Dest   string
+}
+
+var args myargs
+
+func main() {
+	// Get Command Line Variables
+	flag.StringVar(&args.Source, "Source", "", "Source APW File")
+	flag.StringVar(&args.Dest, "Dest", "compile.cfg", "Destination CFG File")
+	flag.Parse()
+
+	// Load in the core APW file
+	a, err := apw.LoadAPW(args.Source)
+	if err != nil {
+		println(`Error Loading APW File: "` + args.Source + `"`)
+		println(err)
+		os.Exit(1)
+	}
+
+	// Process and generate the .cfg
+	b := compile.GenerateCFG(*a)
+
+	// Output to File
+	err = ioutil.WriteFile(args.Dest, b, 0644)
+	if err != nil {
+		println(`Error Writing CFG File: "` + args.Dest + `"`)
+		println(err)
+		os.Exit(1)
+	}
+}
---
+++ compile/cli/go.mod
@@ -0,0 +1,8 @@
+module github.com/soloworks/go-netlinx/compile/cli
+
+go 1.12
+
+require (
+	github.com/soloworks/go-netlinx/apw v0.0.0-20190528105346-d600ac5ae4bc
+	github.com/soloworks/go-netlinx/compile v0.0.0-20190528105346-d600ac5ae4bc
+)
