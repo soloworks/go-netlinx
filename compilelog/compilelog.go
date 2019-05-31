d581c8a74889465f9fd4437d465b0030e8b1e734
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-05-31 22:31:19 +0100

    Processing and working

--- compilelog/compilelog.go
+++ compilelog/compilelog.go
@@ -3,18 +3,89 @@ package compilelog
 import (
 	"bufio"
 	"bytes"
-	"fmt"
+	"strconv"
+	"strings"
 )
 
-// ProcessLog takes a raw log file generated via Netlinx Compiler
+// Process takes a raw log file generated via Netlinx Compiler
 // And returns a more readable version with stats
-func ProcessLog(logData []byte) ([]byte, error) {
+func Process(logData []byte, root string) ([]byte, error) {
+
+	// Variables to hold data
+	var FilesTotal int
+	var FilesTotalWarning int
+	var FilesTotalError int
+	var FileWarning bool
+	var FileError bool
+	var FileName string
+	// String Builder
+	var sb strings.Builder
 
 	// Put the bytes into a scanner
 	scanner := bufio.NewScanner(bytes.NewReader(logData))
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		// Get a line
+		l := scanner.Text()
+		// Trim Whitespace
+		l = strings.TrimSpace(l)
+		// Remove root if present
+		l = strings.Replace(l, root, "", 1)
+		// Only handle lines with more than 1 char
+		if len(l) > 0 {
+			if strings.HasPrefix(l, "---- Starting NetLinx Compile") {
+				FileWarning = false
+				FileError = false
+				FileName = ""
+			}
+			if strings.HasPrefix(l, "WARNING:") || strings.HasPrefix(l, "ERROR:") {
+				if strings.HasPrefix(l, "WARNING:") {
+					FileWarning = true
+					l = strings.TrimPrefix(l, "WARNING:")
+
+				}
+				if strings.HasPrefix(l, "ERROR:") {
+					FileError = true
+					l = strings.TrimPrefix(l, "ERROR:")
+				}
+				l = strings.TrimSpace(l)
+
+				chunks := strings.SplitN(l, "(", 2)
+				if FileName == "" {
+					FileName = chunks[0]
+					sb.WriteString("\n")
+					sb.WriteString(FileName)
+					sb.WriteString("\n")
+				}
+
+				sb.WriteString("(")
+				sb.WriteString(chunks[1])
+				sb.WriteString("\n")
+
+			}
+			if strings.HasPrefix(l, "---- NetLinx Compile Complete") {
+				if FileWarning {
+					FilesTotalWarning++
+				}
+				if FileError {
+					FilesTotalError++
+				}
+				FilesTotal++
+			}
+		}
 	}
 
-	return nil, nil
+	// Add the Stats
+	sb.WriteString("\n")
+	sb.WriteString("\n")
+	sb.WriteString("Files with Warnings: ")
+	sb.WriteString(strconv.Itoa(FilesTotalWarning))
+	sb.WriteString("\n")
+	sb.WriteString("Files with Errors: ")
+	sb.WriteString(strconv.Itoa(FilesTotalError))
+	sb.WriteString("\n")
+	sb.WriteString("Files Processed: ")
+	sb.WriteString(strconv.Itoa(FilesTotal))
+	sb.WriteString("\n")
+
+	return []byte(sb.String()), nil
 }
--- compilelog/go.mod
+++ compilelog/go.mod
@@ -1,3 +1,8 @@
 module github.com/soloworks/go-netlinx/compilelog
 
 go 1.12
+
+require (
+	github.com/soloworks/go-netlinx/apw v0.0.0-20190531201957-cef3f8d7d7d6
+	github.com/soloworks/go-netlinx/compilecfg v0.0.0-20190531201957-cef3f8d7d7d6
+)
